3601d543cd5a6e3583feeab9b59d2b492e5bdb51
joaovitor0330 <45473494+joaovitor0330@users.noreply.github.com>
2025-11-10 21:17:40 -0300

    Update Proj_1-lilblink.ino
    added a comment

--- Proj_1-lilblink.ino
+++ Proj_1-lilblink.ino
@@ -7,7 +7,7 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(red1, HIGH);
+  digitalWrite(red1, HIGH); // could be digitalWrite(red1,!digitalRead(red1));
   delay(1000);
   digitalWrite(red1, LOW);
   delay(1000);
